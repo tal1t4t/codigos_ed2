06f0d11e16e4617bc386adc409e85ff37f49c484
Talita Toledo <talitatoledo3@gmail.com>
2026-07-26 01:06:09 -0300

    passagem de parâmetros

---
+++ pass_params/valor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+void f2(int* y){
+    *y = *y + 2;
+    cout << "y = " << y << endl;
+    cout << "*y = " << *y << endl;
+    cout << "end. de y = " << &y << endl << endl;
+}
+
+void f1(int x){ 
+    x = x + 2;
+    cout << "x = " << x << endl;
+    cout << "end. de x = " << &x << endl << endl;
+}
+
+int main(){
+    int a = 5;
+    int *b = &a;
+
+    cout << "a = " << a << endl;
+    cout << "end. de a = " << &a << endl << endl;
+
+    cout << "b = " << b << endl;
+    cout << "*b = " << *b << endl;
+    cout << "end. de b = " << &b << endl << endl;
+
+    f1(a);
+    f2(b);
+
+    cout << "a = " << a << endl;
+    cout << "end. de a = " << &a << endl << endl;
+
+    cout << "b = " << b << endl;
+    cout << "*b = " << *b << endl;
+    cout << "end. de b = " << &b << endl << endl;
+
+}
