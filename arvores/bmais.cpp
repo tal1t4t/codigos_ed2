e0fe196e56910963774dc857971748f380db1148
Talita Toledo <talitatoledo3@gmail.com>
2026-09-08 14:58:39 -0300

    configurando escolhas de grau da árvore

--- arvores/bmais.cpp
+++ arvores/bmais.cpp
@@ -41,6 +41,19 @@ no* cria_no(){
     return novo;
 }
 
+bool no_cheio(no* n, no* raiz, const int GRAU){
+    for (int i = 0; i < GRAU - 1; i++) {
+        if (n->palavras[i].palavra == ""){
+            return false;
+        }
+    } 
+    return true;   
+}
+
+void balancear(no* &n, no* &raiz, const int GRAU){
+    return;
+}
+
 string limpaPalavra(string palavra){
     return 0; // mudar aqui só quando a função estiver consertada
 }
@@ -84,7 +97,7 @@ bool palavraExcluida(const vector<string>& exclusao, const string& palavra) {
     return binary_search(exclusao.begin(), exclusao.end(), palavra);
 }
 
-void insere(string valor, int linha, no*& n, no*& raiz){
+void insere(string valor, int linha, no*& n, no*& raiz, const int GRAU){
     if (arvore_vazia(n)) {
         n = cria_no();
 
@@ -94,10 +107,9 @@ void insere(string valor, int linha, no*& n, no*& raiz){
 
         return;
     }
-    else {
-        for (int i = 0; i < 3; i++) {
-        
-        }
+    else if(no_cheio(n, raiz, GRAU)){
+        balancear(n, raiz, GRAU);
+
     }
 
 }
@@ -107,7 +119,7 @@ void insere(string valor, int linha, no*& n, no*& raiz){
 // LEITURA DO ARQUIVO
 // ======================================================
 
-void leituraArquivo(string nomeArquivo, no*& raiz, const vector<string>& exclusao){
+void leituraArquivo(string nomeArquivo, no*& raiz, const vector<string>& exclusao, const int GRAU){
     ifstream arquivo(nomeArquivo);
 
     if (!arquivo.is_open()) {
@@ -147,7 +159,7 @@ void leituraArquivo(string nomeArquivo, no*& raiz, const vector<string>& exclusa
                     // Verifica se NÃO é palavra de exclusão
                     if (!palavraExcluida(exclusao, palavra)) {
                         // já insere as palavras na B+
-                        insere(palavra, numeroLinha, raiz, raiz);
+                        insere(palavra, numeroLinha, raiz, raiz, GRAU);
                     }
                     palavra = "";
                 }
@@ -160,7 +172,22 @@ void leituraArquivo(string nomeArquivo, no*& raiz, const vector<string>& exclusa
 }
 
 int main(){
+    int num;
+    cout << "Digite o grau da árvore(3 a 6): " << endl;
+    cin >> num;
+
+    while (num != 3 && num != 4 && num != 5 && num != 6) {
+        cout << "grau inválido." << endl <<
+                "Escolha um grau de 3 a 6 para sua arvore: ";
+        cin >> num;
+    }
+
+    int const GRAU = num;
+
     no* n = cria_arv_vazia();
+    n->ponteiros.resize(GRAU + 1);
+    n->palavras.resize(GRAU - 1);
+
     // ----------------------------------
     // CARREGA LISTA DE EXCLUSÃO
     // ----------------------------------
@@ -170,11 +197,11 @@ int main(){
     // LÊ O TEXTO E MONTA A B+
     // ----------------------------------
 
-    leituraArquivo("texto.txt", n, exclusao);
+    leituraArquivo("texto.txt", n, exclusao, GRAU);
    
     //mostra(n);
     cout << "Arquivo processado!"<< endl
-        << "Arvore B+ criada."<< endl;
+        << "Arvore B+ com grau " << GRAU << "criada."<< endl;
 
     // ----------------------------------
     // CONSULTAS
