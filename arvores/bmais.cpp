a071cca2f4b593ff3161a3b2ed706bee1be7fe3c
Talita Toledo <talitatoledo3@gmail.com>
2026-09-09 19:52:56 -0300

    balanceamento na parte das folhas

--- arvores/bmais.cpp
+++ arvores/bmais.cpp
@@ -6,7 +6,7 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
-#include <chrono>
+#include <chrono> 
 
 using namespace std;
 using namespace std::chrono;
@@ -25,7 +25,7 @@ typedef struct No{
     Assim, sempre teremos uma árvore com grau + 1 ponteiros! */
     vector<No*> ponteiros;
 
-    vector<dados> palavras; // aqui teremos grau - 1 palavras
+    vector<dados> chaves; // aqui teremos grau - 1 chaves
 }no;
 
 no* cria_arv_vazia(){
@@ -52,19 +52,88 @@ bool e_folha(no* n, int GRAU){
 
 bool no_cheio(no* n, no* raiz, const int GRAU){
     for (int i = 0; i < GRAU - 1; i++) {
-        if (n->palavras[i].palavra == ""){
+        if (n->chaves[i].palavra == ""){
             return false;
         }
     } 
     return true;   
 }
 
-void balancear(no* &n, no* &raiz, const int GRAU){
-    return;
+void balancear(no* &n, no* &raiz, const int GRAU, string valor, vector<int> linha){
+    if (e_folha(n, GRAU)) {
+        no* aux1 = new no;
+        no* aux2 = new no;
+        int i;
+        bool entrada_foi = false;
+        // distribuindo os valores nas folhas novas
+        for (i = 0; i < GRAU - 1; i++) {
+            if (i < GRAU/2) {
+                if (valor < n->chaves[i].palavra) {
+                    aux1->chaves[i].palavra = valor;
+                    aux1->chaves[i].linhas = linha;
+                    aux1->chaves[i].contador = 1;
+
+                    entrada_foi = true;
+                    i++;
+                }
+                aux1->chaves[i].palavra = n->chaves[i].palavra;
+                aux1->chaves[i].linhas = n->chaves[i].linhas;
+                aux1->chaves[i].contador = n->chaves[i].contador;
+            }
+            else {
+                if (valor < n->chaves[i].palavra) {
+                    aux2->chaves[abs(GRAU - i - GRAU/2)].palavra = valor;
+                    aux2->chaves[GRAU - i - GRAU/2].linhas = linha;
+                    aux2->chaves[GRAU - i - GRAU/2].contador = 1;
+
+                    entrada_foi = true;
+                    i++;
+                }
+                aux2->chaves[abs(GRAU - i - GRAU/2)].palavra = n->chaves[GRAU - i - GRAU/2].palavra;
+                aux2->chaves[abs(GRAU - i - GRAU/2)].linhas = n->chaves[GRAU - i - GRAU/2].linhas;
+                aux2->chaves[abs(GRAU - i - GRAU/2)].contador =  n->chaves[GRAU - i - GRAU/2].contador;
+            }
+
+            if (i == GRAU - 2 && !entrada_foi) {
+                aux2->chaves[abs(GRAU - i - GRAU/2)].palavra = valor;
+                aux2->chaves[abs(GRAU - i - GRAU/2)].linhas = linha;
+                aux2->chaves[GRAU - i - GRAU/2].contador = 1;
+            }
+        }
+
+    }
+    else {
+        if (GRAU % 2 == 0) {
+            balancear(n->ponteiros[0], raiz, GRAU, n->chaves[GRAU - GRAU/2].palavra, n->chaves[GRAU - GRAU/2].linhas);
+        
+        }
+        else {
+            balancear(n->ponteiros[0], raiz, GRAU, n->chaves[GRAU - GRAU/2 + 1].palavra, n->chaves[GRAU - GRAU/2 + 1].linhas);
+        }
+        /* ordenar os nós intermediários (usar função sort da biblioteca algorithm)
+
+            se o nó estiver cheio, sobe com o menor valor da segunda metade para balancear em cima
+            e apaga do nó atual(só repete na folha)
+
+            ajustar os ponteiros ao ordenar
+        */
+    }
 }
 
 string limpaPalavra(string palavra){
-    return 0; // mudar aqui só quando a função estiver consertada
+    string limpa = "";
+
+    for (char c : palavra) {
+
+        if (isalnum(static_cast<unsigned char>(c))) {
+
+            limpa += tolower(
+                static_cast<unsigned char>(c)
+            );
+        }
+    }
+
+    return limpa;
 }
 
 no* busca(no* n, const string& valor){
@@ -110,15 +179,22 @@ void insere(string valor, int linha, no*& n, no*& raiz, const int GRAU){
     if (arvore_vazia(n)) {
         n = cria_no();
 
-        n->palavras[0].palavra = valor;
-        n->palavras[0].contador = 1;
-        n->palavras[0].linhas.push_back(linha);
+        n->chaves[0].palavra = valor;
+        n->chaves[0].contador = 1;
+        n->chaves[0].linhas.push_back(linha);
 
         return;
     }
     else if(no_cheio(n, raiz, GRAU)){
-        balancear(n, raiz, GRAU);
-
+        vector<int> linhas;
+        linhas.push_back(linha);
+        balancear(n, raiz, GRAU, valor, linhas);
+    }
+    else {//ainda tem espaço pra inserir no nó
+        /*
+            ordenar nó ao inserir 
+        */
+        return;
     }
 
 }
@@ -195,7 +271,7 @@ int main(){
 
     no* n = cria_arv_vazia();
     n->ponteiros.resize(GRAU + 1);
-    n->palavras.resize(GRAU - 1);
+    n->chaves.resize(GRAU - 1);
 
     // ----------------------------------
     // CARREGA LISTA DE EXCLUSÃO
